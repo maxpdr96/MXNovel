653e82a0df117348bbc201f43621108e9793c741
maxpdr96 <maxpdr96@gmail.com>
2020-02-23 16:44:30 -0300

    Web Scraping Title and Text

--- src/mxnovel/core.clj
+++ src/mxnovel/core.clj
@@ -1,6 +1,18 @@
-(ns mxnovel.core)
+(ns mxnovel.core
+  (:require [net.cgrand.enlive-html :as html]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def urlNovel "http://novelmania.com.br/chinesa/ri-indice/ri-capitulo-60/")
+
+(defn fetch-url [url]
+  (html/html-resource (java.net.URL. url)))
+
+(def getHTML (fetch-url urlNovel))
+
+(def getBodyTitle (html/select getHTML [:div.inside-article :h1]))
+(def getTitle (map (comp :content) getBodyTitle))
+
+(def getBodyText (html/select getHTML [:div.entry-content :span]))
+(def getText (map (comp :content) getBodyText))
+
+(println getTitle)
+(println getText)
