221b34d9afe676d4faf9f493694b04833c9f5309
maxpdr96 <maxpdr96@gmail.com>
2020-02-23 16:23:02 -0300

    First Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mxnovel
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/mxnovel/core_test.clj
@@ -0,0 +1,7 @@
+(ns mxnovel.core-test
+  (:require [clojure.test :refer :all]
+            [mxnovel.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
